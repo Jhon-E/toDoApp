a8f8a799112317d37b720e5fece23e85e266bd57
Jhon-E <jhoneospino@gmail.com>
2024-06-18 13:51:20 -0500

    rediseño

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import ListOfTask from "./components/ListOfTasks";
 import { RenderClock } from "./components/RenderClock";
 import { FormTasks } from "./components/FormTasks";
 import useFetch from "./context/useFecth";
+import { useState } from "react";
 
 function App() {
   const { data, loading } = useFetch("https://api.quotable.io/random");
+  const [opc, setOpc] = useState("all");
+
   return (
     <div className="globalContainer">
       <section className="header">
         <header>
-          <RenderClock />
+          <h1>To-do list</h1>
           {loading ? (
             <h4>Loading...</h4>
           ) : data ? (
@@ -23,13 +26,34 @@ function App() {
             <h4>Error</h4>
           )}
         </header>
-        <FormTasks />
       </section>
       {/* ACA VAN LAS TAKS */}
+      <FormTasks />
       <section className="TaskBox">
-        <ListOfTask />
+        <ListOfTask opc={opc} />
       </section>
-      <span style={{ color: "#AAA", marginTop:"2rem"}}>TODO APP by Jhoneiker</span>
+      <ul className="opc">
+        <li
+          onClick={() => setOpc("all")}
+          className={opc == "all" ? "selectOpc" : ""}
+        >
+          All
+        </li>
+        <li
+          onClick={() => setOpc("complete")}
+          className={opc == "complete" ? "selectOpc" : ""}
+        >
+          Checked
+        </li>
+        <li
+          onClick={() => setOpc("incomplete")}
+          className={opc == "incomplete" ? "selectOpc" : ""}
+        >
+          Incomplete
+        </li>
+      </ul>
+      <RenderClock />
+      <footer style={{ color: "#AAA" }}>TODO APP by Jhoneiker</footer>
     </div>
   );
 }
--- src/components/ListOfTasks.jsx
+++ src/components/ListOfTasks.jsx
@@ -1,17 +1,35 @@
 import { RenderTask } from "./RenderTask";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { context } from "../context/tasksContext.jsx";
 
-const ListOfTask = () => {
+const ListOfTask = ({ opc }) => {
   const { tasks } = useContext(context);
+  const [filterTasks, setFilterTasks] = useState(tasks);
+
+  useEffect(() => {
+    switch (opc) {
+      case "complete":
+        setFilterTasks(tasks.filter((t) => t.checked === true));
+        break;
+      case "incomplete":
+        setFilterTasks(tasks.filter((t) => t.checked === false));
+        break;
+      case "all":
+        setFilterTasks(tasks);
+        break;
+      default:
+        throw new Error("Opción invalida");
+    }
+  }, [opc, tasks]);
+
   return tasks.length > 0 ? (
     <ul className="listTasks">
-      {tasks.map((t) => {
+      {filterTasks.map((t) => {
         return <RenderTask key={t.id} task={t} />;
       })}
     </ul>
   ) : (
-    <h1 style={{color:"#555"}}>No tasks.</h1>
+    <h1 style={{ color: "#555" }}>No tasks.</h1>
   );
 };
 
